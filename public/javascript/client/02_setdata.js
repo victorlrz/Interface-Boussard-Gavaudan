f96b622faa5fa8423c987c17f1877f04ec123c5c
Victor <larrezetvictor@gmail.com>
2020-08-06 22:37:40 +0100

    :rocket: settingData

--- public/javascript/client/02_setdata.js
+++ public/javascript/client/02_setdata.js
@@ -13,40 +13,40 @@ const setDataDOM = (data, index) => {
   const relativeDelta = document.querySelector(".relativeDelta");
 
   data[index]["Ticker"].length == 0
-    ? (tckrName.innerHTML = "-")
+    ? (tckrName.innerHTML = "—")
     : (tckrName.innerHTML = data[index]["Ticker"]);
   data[index]["INSTR NAME"].length == 0
-    ? (instrName.innerHTML = "-")
+    ? (instrName.innerHTML = "—")
     : (instrName.innerHTML = data[index]["INSTR NAME"]);
   data[index]["QtyInBook"].length == 0
-    ? (qtyInBook.innerHTML = "-")
+    ? (qtyInBook.innerHTML = "—")
     : (qtyInBook.innerHTML = data[index]["QtyInBook"]);
   data[index]["Last EUR"].length == 0
-    ? (lastEur.innerHTML = "-")
+    ? (lastEur.innerHTML = "—")
     : (lastEur.innerHTML = data[index]["Last EUR"]);
   data[index]["Direction"].length == 0
-    ? (direction.innerHTML = "-")
+    ? (direction.innerHTML = "—")
     : (direction.innerHTML = data[index]["Direction"]);
   data[index]["Discretionnary Adj"].length == 0
-    ? (discrAdj.innerHTML = "-")
+    ? (discrAdj.innerHTML = "—")
     : (discrAdj.innerHTML = data[index]["Discretionnary Adj"]);
   data[index]["To Trade Long"].length == 0
-    ? (toTradeLong.innerHTML = "-")
+    ? (toTradeLong.innerHTML = "—")
     : (toTradeLong.innerHTML = data[index]["To Trade Long"]);
   data[index]["Long Move Date"].length == 0
-    ? (longMoveDate.innerHTML = "-")
+    ? (longMoveDate.innerHTML = "—")
     : (longMoveDate.innerHTML = data[index]["Long Move Date"]);
   data[index]["To Trade Short"].length == 0
-    ? (toTradeShort.innerHTML = "-")
+    ? (toTradeShort.innerHTML = "—")
     : (toTradeShort.innerHTML = data[index]["To Trade Short"]);
   data[index]["Short Move Date"].length == 0
-    ? (shortMoveDate.innerHTML = "-")
+    ? (shortMoveDate.innerHTML = "—")
     : (shortMoveDate.innerHTML = data[index]["Short Move Date"]);
   data[index]["Delta"].length == 0
-    ? (delta.innerHTML = "-")
+    ? (delta.innerHTML = "—")
     : (delta.innerHTML = data[index]["Delta"]);
   data[index]["Relative Delta"].length == 0
-    ? (relativeDelta.innerHTML = "-")
+    ? (relativeDelta.innerHTML = "—")
     : (relativeDelta.innerHTML = data[index]["Relative Delta"]);
 };
 
